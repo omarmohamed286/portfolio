f4c0fe70a21e47878d1b6dc914777796ef3ebc8d
Omar Mohamed <125928590+omarmohamed286@users.noreply.github.com>
2025-06-05 04:53:03 +0300

    astro site added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.omarmohamed.dev",
   vite: {
-    site:"https://www.omarmohamed.dev",
     plugins: [tailwindcss()],
   },
 });
